4aecb28a2f1144d3f83a910e21e8a51f849f2c50
Fabian Jülich <63878623+fabianjuelich@users.noreply.github.com>
2023-09-18 14:46:34 +0200

    Bugfixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import tkinter as tk
+from tktooltip import ToolTip
 from PIL import Image
 import requests
 from io import BytesIO
@@ -11,19 +12,19 @@ from threading import Thread
 
 PLATFORM = platform.system()
 
-logging.basicConfig(level = logging.INFO, filename='.EDOpro_SD.log', filemode='w', format='%(levelname)s: %(asctime)s: %(message)s')
+logging.basicConfig(level = logging.INFO, filename='.EDOPro_SD.log', filemode='w', format='%(levelname)s: %(asctime)s: %(message)s')
 
 def abs_path(path): return os.path.join(os.path.dirname(__file__), path)
 
 PROJECT_IGNIS_DEFAULT = os.path.join(os.path.abspath(os.sep), 'ProjectIgnis')
 config = ConfigParser()
-if not os.path.exists(('.EDOpro_SD.ini')):
+if not os.path.exists(('.EDOPro_SD.ini')):
     config.add_section('ProjectIgnis')
     config.set('ProjectIgnis', 'path', PROJECT_IGNIS_DEFAULT)
-    with open(('.EDOpro_SD.ini'), 'w') as ini:
+    with open(('.EDOPro_SD.ini'), 'w') as ini:
         config.write(ini)
     logging.info('Default config-file created')
-config.read(('.EDOpro_SD.ini'))
+config.read(('.EDOPro_SD.ini'))
 logging.info(dict(map(lambda s: (s, dict(config.items(s))), config.keys())))
 PROJECT_IGNIS = config.get('ProjectIgnis', 'path')
 
@@ -37,18 +38,25 @@ unlock = False
 cnt = 1
 selected = None
 
+def enableInput(enable):
+    combobox.configure(state=('normal' if enable else 'disabled'))
+    button.configure(state=('normal' if enable else 'disabled'))
+
+def default_combobox():
+    combobox.configure(values=list(map(lambda sd: sd[0], set_list.items())))
+
 def show_image(sd):
     global selected
     if sd:
         selected = sd
         img = ctk.CTkImage(Image.open(BytesIO(requests.get(sd['set_image']).content)), size=(100, 164))
-        image_holder.configure(image=img, state='normal')
+        button.configure(image=img, state='normal')
         if f'{sd["set_name"].replace(":", "")}.ydk' in os.listdir(os.path.join(PROJECT_IGNIS, 'deck')):
             label.configure(text='Already added')
         else:
             label.configure(text='Click deck to add')
     else:
-        image_holder.configure(image=logo, state='disabled')
+        button.configure(image=logo, state='disabled')
         selected = None
         label.configure(text='Choose deck')
 
@@ -60,18 +68,24 @@ def search(event):
             results.append(set_list[set])
     combobox.configure(values=list(map(lambda r: r['set_name'], results)))
     if len(results) == 1:
-        show_image(results[0])
+        if results[0] != selected:
+            show_image(results[0])
     else:
-        show_image(None)
+        if selected:
+            show_image(None)
 
-def unfold(event):
+def select(event):
     global unlock
-    combobox.configure(values=list(map(lambda sd: sd[0], set_list.items())))
-    show_image(set_list[combobox.get()])
+    default_combobox()
+    if set_list[combobox.get()] != selected:
+        show_image(set_list[combobox.get()])
     unlock = True
     app.focus()
 
 def save_background():
+    global prompt
+    combobox.set(selected['set_name'])
+    enableInput(False)
     label.configure(text='Please wait...')
     app.update()
     passwords = {'main': [], 'extra': []}
@@ -88,8 +102,12 @@ def save_background():
     except Exception as e:
         logging.error(e)
         label.configure(text='Something went wrong')
+    prompt = True
+    app.focus()
+    enableInput(True)
+    default_combobox()
 
-def save():
+def save(event=None):
     Thread(target=save_background).start()
 
 def delete(event):
@@ -106,22 +124,25 @@ def delete(event):
         cnt = 0
 
 app = ctk.CTk()
-app.title('EDOpro SD')
+app.title('EDOPro SD')
 icon = tk.PhotoImage(file=abs_path('assets/icons8-card-game-66.png'))
 app.iconphoto(True, icon)
 
-combobox = ctk.CTkComboBox(app, values=list(map(lambda sd: sd[0], set_list.items())), command=unfold)
+combobox = ctk.CTkComboBox(app, values=list(map(lambda sd: sd[0], set_list.items())), command=select)
 combobox.set('Search')
 
-logo = ctk.CTkImage(Image.open(abs_path('assets/EDOpro_logo.png')), size=(100, 164))
-image_holder = ctk.CTkButton(app, text='', fg_color='transparent', image=logo, state='disabled', command=save)
+logo = ctk.CTkImage(Image.open(abs_path('assets/EDOPro-logo.png')), size=(100, 164))
+button = ctk.CTkButton(app, text='', fg_color='transparent', image=logo, state='disabled', command=save)
 label = ctk.CTkLabel(app, text='Choose deck')
 
 combobox.pack()
-image_holder.pack()
+button.pack()
 label.pack()
 
 combobox.bind('<KeyRelease>', search)
+combobox.bind('<Return>', save)
 combobox.bind('<FocusIn>', delete)
+
+ToolTip(button, msg=lambda: selected['set_name'] if selected else '', bg='#1c1c1c', fg='#ffffff')
 
 app.mainloop()
