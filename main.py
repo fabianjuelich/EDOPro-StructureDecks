51a1e42127f409f66ad8de99b07424d79fcf8a2f
Fabian Jülich <63878623+fabianjuelich@users.noreply.github.com>
2023-10-28 00:43:06 +0200

    Refactor

--- main.py
+++ main.py
@@ -32,117 +32,117 @@ SET_LIST = 'https://db.ygoprodeck.com/api/v7/cardsets.php'
 CARD_LIST = 'https://yugiohprices.com/api/set_data/'
 CARD_INFO = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name='
 
-set_list = dict(filter(lambda sd: bool(sd), list(map(lambda obj: (obj['set_name'], obj) if 'structure deck' in obj['set_name'].lower() else {}, requests.get(SET_LIST).json()))))
-prompt = True
-unlock = False
-cnt = 1
-selected = None
+class App(ctk.CTk):
 
-def enableInput(enable):
-    combobox.configure(state=('normal' if enable else 'disabled'))
-    button.configure(state=('normal' if enable else 'disabled'))
+    def __init__(self):
 
-def default_combobox():
-    combobox.configure(values=list(map(lambda sd: sd[0], set_list.items())))
+        super().__init__()
 
-def show_image(sd):
-    global selected
-    if sd:
-        selected = sd
-        img = ctk.CTkImage(Image.open(BytesIO(requests.get(sd['set_image']).content)), size=(100, 164))
-        button.configure(image=img, state='normal')
-        if f'{sd["set_name"].replace(":", "")}.ydk' in os.listdir(os.path.join(PROJECT_IGNIS, 'deck')):
-            label.configure(text='Already added')
-        else:
-            label.configure(text='Click deck to add')
-    else:
-        button.configure(image=logo, state='disabled')
-        selected = None
-        label.configure(text='Choose deck')
+        self.set_list = dict(filter(lambda sd: bool(sd), list(map(lambda obj: (obj['set_name'], obj) if 'structure deck' in obj['set_name'].lower() else {}, requests.get(SET_LIST).json()))))
 
-def search(event):
-    global selected
-    results = []
-    for set in set_list:
-        if combobox.get().strip().lower() in set.lower():
-            results.append(set_list[set])
-    combobox.configure(values=list(map(lambda r: r['set_name'], results)))
-    if len(results) == 1:
-        if results[0] != selected:
-            show_image(results[0])
-    else:
-        if selected:
-            show_image(None)
+        self.title('EDOPro SD')
+        icon = tk.PhotoImage(file=abs_path('assets/icons8-card-game-66.png'))
+        self.iconphoto(True, icon)
 
-def select(event):
-    global unlock
-    default_combobox()
-    if set_list[combobox.get()] != selected:
-        show_image(set_list[combobox.get()])
-    unlock = True
-    app.focus()
+        self.combobox = ctk.CTkComboBox(self, values=list(map(lambda sd: sd[0], self.set_list.items())), command=self.select)
+        self.combobox.set('Search')
+        self.logo = ctk.CTkImage(Image.open(abs_path('assets/EDOPro_logo.png')), size=(100, 164))
+        self.button = ctk.CTkButton(self, text='', fg_color='transparent', image=self.logo, state='disabled', command=self.save)
+        self.label = ctk.CTkLabel(self, text='Choose deck')
 
-def save_background():
-    global prompt
-    combobox.set(selected['set_name'])
-    enableInput(False)
-    label.configure(text='Please wait...')
-    app.update()
-    passwords = {'main': [], 'extra': []}
-    try:
-        for card in ((requests.get(CARD_LIST+selected['set_name'])).json()['data']['cards']):
-            card_info = (requests.get(CARD_INFO+(card['name']).replace('&', '%26')).json())
-            if 'data' in card_info:
-                passwords['extra' if card_info['data'][0]['frameType'] in ['fusion', 'synchro', 'xyz', 'link'] else 'main'].append(str(card_info['data'][0]['id']))
+        self.combobox.pack()
+        self.button.pack()
+        self.label.pack()
+
+        self.combobox.bind('<KeyRelease>', self.search)
+        self.combobox.bind('<Return>', self.save)
+        self.combobox.bind('<FocusIn>', self.delete)
+
+        self.prompt = True
+        self.unlock = False
+        self.cnt = 1
+        self.selected = None
+
+        ToolTip(self.button, msg=lambda: self.selected['set_name'] if self.selected else '', bg='#1c1c1c', fg='#ffffff')
+
+    def enableInput(self, enable):
+        self.combobox.configure(state=('normal' if enable else 'disabled'))
+        self.button.configure(state=('normal' if enable else 'disabled'))
+
+    def default_combobox(self):
+        self.combobox.configure(values=list(map(lambda sd: sd[0], self.set_list.items())))
+
+    def show_image(self, sd):
+        if sd:
+            self.selected = sd
+            img = ctk.CTkImage(Image.open(BytesIO(requests.get(sd['set_image']).content)), size=(100, 164))
+            self.button.configure(image=img, state='normal')
+            if f'{sd["set_name"].replace(":", "")}.ydk' in os.listdir(os.path.join(PROJECT_IGNIS, 'deck')):
+                self.label.configure(text='Already added')
             else:
-                logging.warning(f'"{card["name"]}" not found')
-        with open(os.path.join(PROJECT_IGNIS, 'deck', f'{selected["set_name"].replace(":", "")}.ydk'), 'w') as deck:
-            deck.write('#created by Player\n'+'#main\n'+'\n'.join(passwords['main'])+'\n#extra\n'+'\n'.join(passwords['extra'])+'\n!side')
-        label.configure(text='Deck added')
-    except Exception as e:
-        logging.error(e)
-        label.configure(text='Something went wrong')
-    prompt = True
-    app.focus()
-    enableInput(True)
-    default_combobox()
+                self.label.configure(text='Click deck to add')
+        else:
+            self.button.configure(image=self.logo, state='disabled')
+            self.selected = None
+            self.label.configure(text='Choose deck')
 
-def save(event=None):
-    Thread(target=save_background).start()
+    def search(self, event):
+        self.selected
+        results = []
+        for set in self.set_list:
+            if self.combobox.get().strip().lower() in set.lower():
+                results.append(self.set_list[set])
+        self.combobox.configure(values=list(map(lambda r: r['set_name'], results)))
+        if len(results) == 1:
+            if results[0] != self.selected:
+                self.show_image(results[0])
+        else:
+            if self.selected:
+                self.show_image(None)
 
-def delete(event):
-    global prompt
-    global unlock
-    global cnt
-    if unlock:
-        cnt += 1
-    if prompt or (unlock and cnt==(2 if PLATFORM == 'Linux' else 1)):
-        combobox.set('')
-        show_image(None)
-        prompt = False
-        unlock = False
-        cnt = 0
+    def select(self, event):
+        self.default_combobox()
+        if self.set_list[self.combobox.get()] != self.selected:
+            self.show_image(self.set_list[self.combobox.get()])
+        self.unlock = True
+        self.focus()
 
-app = ctk.CTk()
-app.title('EDOPro SD')
-icon = tk.PhotoImage(file=abs_path('assets/icons8-card-game-66.png'))
-app.iconphoto(True, icon)
+    def save_background(self):
+        self.combobox.set(self.selected['set_name'])
+        self.enableInput(False)
+        self.label.configure(text='Please wait...')
+        self.update()
+        passwords = {'main': [], 'extra': []}
+        try:
+            for card in ((requests.get(CARD_LIST+self.selected['set_name'])).json()['data']['cards']):
+                card_info = (requests.get(CARD_INFO+(card['name']).replace('&', '%26')).json())
+                if 'data' in card_info:
+                    passwords['extra' if card_info['data'][0]['frameType'] in ['fusion', 'synchro', 'xyz', 'link'] else 'main'].append(str(card_info['data'][0]['id']))
+                else:
+                    logging.warning(f'"{card["name"]}" not found')
+            with open(os.path.join(PROJECT_IGNIS, 'deck', f'{self.selected["set_name"].replace(":", "")}.ydk'), 'w') as deck:
+                deck.write('#created by Player\n'+'#main\n'+'\n'.join(passwords['main'])+'\n#extra\n'+'\n'.join(passwords['extra'])+'\n!side')
+            self.label.configure(text='Deck added')
+        except Exception as e:
+            logging.error(e)
+            self.label.configure(text='Something went wrong')
+        self.prompt = True
+        self.focus()
+        self.enableInput(True)
+        self.default_combobox()
 
-combobox = ctk.CTkComboBox(app, values=list(map(lambda sd: sd[0], set_list.items())), command=select)
-combobox.set('Search')
+    def save(self, event=None):
+        Thread(target=self.save_background).start()
 
-logo = ctk.CTkImage(Image.open(abs_path('assets/EDOPro_logo.png')), size=(100, 164))
-button = ctk.CTkButton(app, text='', fg_color='transparent', image=logo, state='disabled', command=save)
-label = ctk.CTkLabel(app, text='Choose deck')
+    def delete(self, event):
+        if self.unlock:
+            self.cnt += 1
+        if self.prompt or (self.unlock and self.cnt==(2 if PLATFORM == 'Linux' else 1)):
+            self.combobox.set('')
+            self. show_image(None)
+            self.prompt = False
+            self.unlock = False
+            self.cnt = 0
 
-combobox.pack()
-button.pack()
-label.pack()
-
-combobox.bind('<KeyRelease>', search)
-combobox.bind('<Return>', save)
-combobox.bind('<FocusIn>', delete)
-
-ToolTip(button, msg=lambda: selected['set_name'] if selected else '', bg='#1c1c1c', fg='#ffffff')
-
+app = App()
 app.mainloop()
